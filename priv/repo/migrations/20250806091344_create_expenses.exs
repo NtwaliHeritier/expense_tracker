b5c65584eac7cc4ad82a1e371ee629991b7cce88
ntwali <ntwalihatsor78.nh@gmail.com>
2025-08-06 11:27:30 +0200

    adds expense schema

---
+++ priv/repo/migrations/20250806091344_create_expenses.exs
@@ -0,0 +1,17 @@
+defmodule ExpenseTracker.Repo.Migrations.CreateExpenses do
+  use Ecto.Migration
+
+  def change do
+    create table(:expenses) do
+      add :description, :text, null: false
+      add :amount, :decimal, null: false
+      add :date, :date, null: false
+      add :notes, :text
+      add :category_id, references(:categories, on_delete: :nilify_all), null: false
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create index(:expenses, [:category_id])
+  end
+end
